378295da721f5b5502b2d732b38a9d121fcda634
luke-strange <lukestrange123@gmail.com>
2024-10-02 23:54:32 +0100

    Update navgrid

---
+++ _cms.ts
@@ -0,0 +1,11 @@
+import lumeCMS from "lume/cms/mod.ts";
+
+const cms = lumeCMS({
+    site: {
+      name: "Luke Strange Portfolio CMS",
+      description: "Edit the content of my site",
+      url: "https://luke-strange.github.io/portfolio/",
+    },
+  });
+
+export default cms;
